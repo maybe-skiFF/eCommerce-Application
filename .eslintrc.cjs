ab5ea81f875d7b61270893c27f6e33bb3447dbb9
maybe-skiFF <den.skiff190@gmail.com>
2024-04-29 23:49:41 +0300

    feat: add husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,59 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'airbnb',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//     'plugin:prettier/recommended',
+//     'prettier'
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "airbnb-base",
+    "airbnb-typescript",
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // 'react/no-restricted-strings': [
+    //   'error',
+    //   {
+    //     message: 'Использование кириллических символов запрещено',
+    //     patterns: ['с'],
+    //   },
+    // ],
   },
-}
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <button>
+      <h1>Welcome to my app</h1>
+    </button>
+  );
+};
