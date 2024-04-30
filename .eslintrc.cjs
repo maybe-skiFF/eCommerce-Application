102a1b6d91b1e1645929cb94447a5cdad0093413
maybe-skiFF <den.skiff190@gmail.com>
2024-04-30 18:49:12 +0300

    feat: add folder structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,6 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    // 'airbnb',
-    // 'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
