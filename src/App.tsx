6e680a4d7e07100c40a5d25a39150c7eb7e5ee44
Aleksa013 <velma86@bk.ru>
2024-05-05 16:36:30 +0300

    feat: add component SignInSide from MaterialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
-import { Button } from './components/UI/Button';
-
+// import { Button } from './components/UI/Button';
+import SignInSide from './components/UI/SignInSide';
 function App() {
   return (
     <>
-      <div>
-        <Button>Here we are</Button>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SignInSide />
     </>
   );
 }
