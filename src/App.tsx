c083f7d1956b191848cd58873e751d7b9fcbc2d5
maybe-skiFF <den.skiff190@gmail.com>
2024-05-06 22:12:56 +0300

    feat: add routes and some styles for 404Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { MainPage } from './components/pages/MainPage';
 import { LoginPage } from './components/pages/LoginPage';
@@ -8,7 +8,16 @@ import { Page404 } from './components/pages/404Page';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/pages/404Page.tsx
+++ src/components/pages/404Page.tsx
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import '../styles/404Page.css';
+
 export function Page404() {
   return (
-    <div>
-      <h1>Page 404</h1>
+    <div className="page404Wrapper">
+      <img
+        className="Page404Img"
+        src="../src/assets/image/404-image.jpeg"
+        alt="404-image"
+      />
+      <p className="Page404Text">
+        The page you are looking for might be removed or is temporarily
+        unavailable
+      </p>
+      <Link to="/">
+        <Button variant="contained">Back To MainPage</Button>
+      </Link>
     </div>
   );
 }
