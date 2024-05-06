2f9643cc7f4b5c7a5d1507cc8fc7935ce8a5a513
maybe-skiFF <den.skiff190@gmail.com>
2024-05-06 20:49:06 +0300

    feat: add react-router-dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import './App.css';
-import { Button } from './components/UI/Button';
 
 function App() {
-  return (
-    <>
-      <div>
-        <Button>Here we are</Button>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
