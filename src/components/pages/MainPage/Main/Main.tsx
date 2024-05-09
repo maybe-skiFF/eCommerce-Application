b1635f96ff1f3deaa5728acf3c1f8b39058a15c6
albert <xsoularisx@gmail.com>
2024-05-09 18:16:05 +0300

    feat: add main

--- src/components/pages/MainPage/Main/Main.tsx
+++ src/components/pages/MainPage/Main/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import './Main.scss';
+import { Button } from '@mui/material';
 
 export default function Main() {
   const [selectedGender, setAlignment] = React.useState('for women');
@@ -17,6 +18,7 @@ export default function Main() {
     <main>
       <ChoiceGender selectedGender={selectedGender} handleChange={handleChange} />
       <ShowMainImage selectedGender={selectedGender} />
+      <ShowShopCard />
     </main>
   );
 }
@@ -45,6 +47,67 @@ function ShowMainImage({ selectedGender }: { selectedGender: string }) {
     <div className="navigation__image">
       {selectedGender === 'for men' && <h2 className='navigation__title'>FOR MEN</h2>}
       {selectedGender === 'for women' && <h2 className='navigation__title'>FOR WOMEN</h2>}
+    </div>
+  );
+}
+
+const items = [{
+  id: '1',
+  image: 'image-1',
+  title: 'name-1',
+  text: 'text-1',
+  price: '1'
+},
+{
+  id: '2',
+  image: 'image-2',
+  title: 'name-2',
+  text: 'text-2',
+  price: '2'
+},
+{
+  id: '3',
+  image: 'image-3',
+  title: 'name-3',
+  text: 'text-3',
+  price: '3'
+},
+{
+  id: '4',
+  image: 'image-4',
+  title: 'name-4',
+  text: 'text-4',
+  price: '4'
+}
+  ,
+{
+  id: '5',
+  image: 'image-5',
+  title: 'name-5',
+  text: 'text-5',
+  price: '5'
+},
+{
+  id: '6',
+  image: 'image-6',
+  title: 'name-6',
+  text: 'text-6',
+  price: '6'
+}
+]
+
+function ShowShopCard() {
+  return (
+    <div className="shop__list">
+      {items.map((item) => (
+        <section className="shop__item" key={item.id}>
+          <div className="item__image">{item.image}</div>
+          <h3 className="item__title">{item.title}</h3>
+          <p className="item__text">{item.text}</p>
+          <p className="item__price">{item.price}</p>
+          <Button className="item__button" variant="outlined">add to cart</Button>
+        </section>
+      ))}
     </div>
   );
 }
