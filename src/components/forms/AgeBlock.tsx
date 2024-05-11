220c6180903f8696911927daf1683af438816373
Aleksa013 <velma86@bk.ru>
2024-05-12 02:32:29 +0300

    feat: add function creating years

--- src/components/forms/AgeBlock.tsx
+++ src/components/forms/AgeBlock.tsx
@@ -11,11 +11,18 @@ import {
 import { SERVICE_MESSAGES, months } from 'src/constants/SERVICE_MESSAGES';
 
 const days: number[] = [];
+const years: number[] = [];
 for (let i = 1; i < 32; i++) {
   days.push(i);
 }
+for (let i = 2024; i > 1924; i--) {
+  years.push(i);
+}
+
 export const AgeBlock = () => {
-  const [age, setAge] = useState('');
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -24,8 +31,14 @@ export const AgeBlock = () => {
       password: data.get('password'),
     });
   };
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+  const handleDayChange = (event: SelectChangeEvent) => {
+    setDay(event.target.value);
+  };
+  const handleMonthChange = (event: SelectChangeEvent) => {
+    setMonth(event.target.value);
+  };
+  const handleYearChange = (event: SelectChangeEvent) => {
+    setYear(event.target.value);
   };
   const getDaysItems = days.map(day => {
     return (
@@ -42,21 +55,27 @@ export const AgeBlock = () => {
       </MenuItem>
     );
   });
+  const getYearsItems = years.map((year, index) => {
+    return (
+      <MenuItem value={index} key={`year${year}`}>
+        {year}
+      </MenuItem>
+    );
+  });
   return (
     <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
       <Grid container spacing={2}>
-        <FormControl sx={{ m: 1, minWidth: 90 }} size="small">
-          <InputLabel id="demo-select-small-label" sx={{ fontSize: '50%' }}>
-            {' '}
+        <FormControl sx={{ m: 1, minWidth: 50 }} size="small">
+          <InputLabel id="day" sx={{ fontSize: '50%' }}>
             {SERVICE_MESSAGES.day}
           </InputLabel>
           <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            value={age}
-            label="Age"
+            labelId="day"
+            id="day"
+            value={day}
+            label="day"
             sx={{ fontSize: '50%' }}
-            onChange={handleChange}
+            onChange={handleDayChange}
           >
             <MenuItem value="">
               <em>None</em>
@@ -64,18 +83,17 @@ export const AgeBlock = () => {
             {getDaysItems}
           </Select>
         </FormControl>
-        <FormControl sx={{ m: 1, minWidth: 90 }} size="small">
-          <InputLabel id="demo-select-small-label" sx={{ fontSize: '50%' }}>
-            {' '}
+        <FormControl sx={{ m: 1, minWidth: 50 }} size="small">
+          <InputLabel id="month" sx={{ fontSize: '50%' }}>
             {SERVICE_MESSAGES.month}
           </InputLabel>
           <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            value={age}
-            label="Age"
+            labelId="month"
+            id="month"
+            value={month}
+            label="month"
             sx={{ fontSize: '50%' }}
-            onChange={handleChange}
+            onChange={handleMonthChange}
           >
             <MenuItem value="">
               <em>None</em>
@@ -83,25 +101,22 @@ export const AgeBlock = () => {
             {getMonthsItems}
           </Select>
         </FormControl>
-        <FormControl sx={{ m: 1, minWidth: 90 }} size="small">
-          <InputLabel id="demo-select-small-label" sx={{ fontSize: '50%' }}>
-            {' '}
+        <FormControl sx={{ m: 1, minWidth: 50 }} size="small">
+          <InputLabel id="year" sx={{ fontSize: '50%' }}>
             {SERVICE_MESSAGES.year}
           </InputLabel>
           <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            value={age}
-            label="Age"
+            labelId="year"
+            id="year"
+            value={year}
+            label="year"
             sx={{ fontSize: '50%' }}
-            onChange={handleChange}
+            onChange={handleYearChange}
           >
             <MenuItem value="">
               <em>None</em>
             </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            {getYearsItems}
           </Select>
         </FormControl>
       </Grid>
