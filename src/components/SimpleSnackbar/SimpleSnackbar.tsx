37adee8f2f346838a1f7c773f18af6cf5ddae80b
maybe-skiFF <den.skiff190@gmail.com>
2024-05-22 23:12:52 +0300

    reafactor: SimpleSnackbar component

--- src/components/SimpleSnackbar/SimpleSnackbar.tsx
+++ src/components/SimpleSnackbar/SimpleSnackbar.tsx
@@ -1,4 +1,4 @@
-import { Fragment, SyntheticEvent } from 'react';
+import { SyntheticEvent } from 'react';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
@@ -10,7 +10,7 @@ export function SimpleSnackbar(
   callback: (event: SyntheticEvent | Event, reason?: string) => void,
 ) {
   const action = (
-    <Fragment>
+    <>
       <Button color="secondary" size="small" onClick={callback}>
         {message}
       </Button>
@@ -22,19 +22,17 @@ export function SimpleSnackbar(
       >
         <CloseIcon fontSize="small" />
       </IconButton>
-    </Fragment>
+    </>
   );
 
   return (
-    <div>
-      <Snackbar
-        style={{ marginLeft: '14%', marginBottom: '15%' }}
-        open={open}
-        autoHideDuration={5000}
-        onClose={callback}
-        message={message}
-        action={action}
-      />
-    </div>
+    <Snackbar
+      style={{ marginLeft: '14%', marginBottom: '15%' }}
+      open={open}
+      autoHideDuration={5000}
+      onClose={callback}
+      message={message}
+      action={action}
+    />
   );
 }
