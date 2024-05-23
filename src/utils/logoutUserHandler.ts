51dd9673148bd34a14bab86fb0888428db4d38ab
maybe-skiFF <den.skiff190@gmail.com>
2024-05-23 21:10:04 +0300

    refactor: removed unused function

--- src/utils/logoutUserHandler.ts
+++
@@ -1,8 +0,0 @@
-function logoutUserHandler() {
-  localStorage.setItem('isAuth', 'false');
-  const logoutBtn = document.querySelector('.logout')!;
-  logoutBtn.classList.remove('logout__btn-active');
-  logoutBtn.classList.add('logout__btn');
-}
-
-export { logoutUserHandler };
